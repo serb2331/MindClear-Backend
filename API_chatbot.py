8e0e52e901d580ebd375d5d80bc1ca835ac01330
serb2331 <serban.foica@gmail.com>
2024-05-26 09:16:41 +0300

    change chatbot to class for denis :)

--- API_chatbot.py
+++ API_chatbot.py
@@ -1,38 +1,48 @@
 import os
 from openai import AzureOpenAI
 
-client = AzureOpenAI(
-  azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
-  api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
-  api_version="2024-02-01"
-)
+class ChatBot:
+    def __init(self):
+        self._client = AzureOpenAI(
+        azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
+        api_version="2024-02-01"
+        )
 
-gpt_prompt=""
-with open("gpt-prompt.txt") as file:
-    prompt_lines = file.readlines()
-    for line in prompt_lines:
-        gpt_prompt += line + "\n"
+        self._gpt_prompt=""
+        with open("gpt-prompt.txt") as file:
+            prompt_lines = file.readlines()
+            for line in prompt_lines:
+                self._gpt_prompt += line + "\n"
+        self._conversation=[{"role": "system", "content": self._gpt_prompt}]
 
-conversation=[{"role": "system", "content": gpt_prompt}]
-
-def generateResponse(user_input: str) -> str:
-    conversation.append({"role": "system", "content": user_input})
+    def startConversation(self) -> str:
+        initial = self._client.chat.completions.create(
+            model=os.getenv("AZURE_OPENAI_MODEL"),
+            messages=self._conversation,
+            temperature=1.5
+        )
+        self._conversation.append({"role": "assistant", "content": initial.choices[0].message.content})
+        return initial.choices[0].message.content
     
-    question = client.chat.completions.create(
-        model=os.getenv("AZURE_OPENAI_MODEL"),
-        messages=conversation,
-        temperature=1.5
-    )
-    conversation.append({"role": "assistant", "content": question.choices[0].message.content})
-    return question.choices[0].message.content
+    def generateResponse(self, user_input: str) -> str:
+        self._conversation.append({"role": "user", "content": user_input})
+        
+        question = self._client.chat.completions.create(
+            model=os.getenv("AZURE_OPENAI_MODEL"),
+            messages=self._conversation,
+            temperature=1.5
+        )
+        self._conversation.append({"role": "assistant", "content": question.choices[0].message.content})
+        return question.choices[0].message.content
 
-def finishConversation() -> int:
-    conversation.append({"role": "system", "content": "done"})
-    
-    question = client.chat.completions.create(
-        model=os.getenv("AZURE_OPENAI_MODEL"),
-        messages=conversation,
-        temperature=0.2
-    )
-    
-    return int(question.choices[0].message.content)
+    def finishConversation(self) -> int:
+        self._conversation.append({"role": "user", "content": "done"})
+        
+        question = self._client.chat.completions.create(
+            model=os.getenv("AZURE_OPENAI_MODEL"),
+            messages=self._conversation,
+            temperature=0.2
+        )
+        
+        return int(question.choices[0].message.content)
